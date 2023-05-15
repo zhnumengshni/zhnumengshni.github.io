d34bbc9eb4fca51fe414057c076c6291b4d5ef0c
zhnumengshni <2235743848@qq.com>
2023-05-15 11:57:13 +0800

    Site updated: 2023-05-15 11:57:13

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+//禁止查看网页源代码
+document.onkeydown = function (e) {
+    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("你真坏，不能打开控制台喔!"), event.keyCode = 0, event.returnValue = !1, !1
+};
+
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = '你跑哪里去了~';
+        clearTimeout(titleTime);
+    } else {
+        //返回当前页面时标签显示内容
+        document.title = '嘿嘿，抓到你啦～';
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
